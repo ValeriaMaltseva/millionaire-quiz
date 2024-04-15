f62cde8d6a2b5578923814a0f722ade10c27918b
Valeriia Maltseva <lerok.maltseva96@gmail.com>
2024-04-15 12:10:20 +0200

    styled GameOver page

--- src/routes/GameOver/GameOver.tsx
+++ src/routes/GameOver/GameOver.tsx
@@ -2,10 +2,16 @@
 
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import Link from 'next/link';
 
 import ROUTES from '@/constants/routes';
+
 import { useGame } from '@/hooks/useGame';
+
+import { USDFormatter } from '@/utils/currency';
+
+import ButtonLink from '@/components/ButtonLink';
+
+import { ReactComponent as HandIcon } from './img/hand.svg';
 
 import styles from './GameOver.module.scss';
 
@@ -23,9 +29,20 @@ function GameOver() {
     return (
         game.isFinished && (
             <section className={styles.root}>
-                <div>GameOver</div>
-                <div>Score: {game.currentScore}</div>
-                <Link href={ROUTES.GAME}>To Start</Link>
+                <div className={styles.container}>
+                    <div className={styles.iconContainer}>
+                        <HandIcon className={styles.icon} />
+                    </div>
+                    <div className={styles.infoContainer}>
+                        <div>
+                            <p className={styles.scoreTitle}>Total score:</p>
+                            <h1 className={styles.scoreInfo}>{USDFormatter(game.currentScore)} earned</h1>
+                        </div>
+                        <ButtonLink href={ROUTES.GAME} className={styles.button}>
+                            Try again
+                        </ButtonLink>
+                    </div>
+                </div>
             </section>
         )
     );
