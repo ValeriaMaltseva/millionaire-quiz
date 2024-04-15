680786dabc616007391bcb3b6e333e157694cc1f
Valeriia Maltseva <lerok.maltseva96@gmail.com>
2024-04-15 23:16:33 +0200

    fixed build

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="next-plugin-svgr/types/svg" />
 /// <reference types="next-plugin-svgr/types/svgFileLoader" />
+
+declare module '*.svg' {
+    import React = require('react');
+
+    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+    const src: string;
+    export default src;
+}
